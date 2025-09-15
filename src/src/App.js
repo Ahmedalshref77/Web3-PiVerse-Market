ede34705feeb1b0688f950dec4d993bd16e9554c
Ahmed Mahmoud Hassan Khalil <ahmedalshref77@gmail.com>
2025-09-16 02:01:50 +0300

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
